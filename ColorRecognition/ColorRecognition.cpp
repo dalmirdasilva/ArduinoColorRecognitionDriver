bd0900ae2be9f6d807cc1e8e5d3f6c2cb0b32c02
Dalmir Silva <dalmirdasilva@gmail.com>
2012-10-07 18:36:32 -0300

    Adding code

---
+++ ColorRecognition/ColorRecognition.cpp
@@ -0,0 +1,16 @@
+/**
+ * Arduino - Color Recognition Sensor
+ * 
+ * ColorRecognition.h
+ * 
+ * The abstract class for the color recognition sensors.
+ * 
+ * @author Dalmir da Silva <dalmirdasilva@gmail.com>
+ */
+
+#ifndef __ARDUINO_DRIVER_COLOR_RECOGNITION_CPP__
+#define __ARDUINO_DRIVER_COLOR_RECOGNITION_CPP__ 1
+
+#include "ColorRecognition.h"
+
+#endif /* __ARDUINO_DRIVER_COLOR_RECOGNITION_CPP__ */
